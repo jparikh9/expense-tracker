26e97c8a942cb01a8a8fe786d409a07bdb39a8f0
jinit <jparikh9@hawk.iit.edu>
2023-07-06 17:01:34 -0500

    work done till now.A static page showing name,cost

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+import Card from '../UI/Card';
+
+function ExpenseItem(props) {
+    //const expenseDate = new Date(2023, 6, 24);
+    //const expenseTitle = 'Car Insurance';
+    //const expenseAmount = 280.67;
+    //const month = props.date.toLocaleString('en-US', { month: 'long' });
+    //const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    //const year = props.date.getFullYear();
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    );
+}
+export default ExpenseItem;
