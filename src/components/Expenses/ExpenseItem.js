327fbe57c102c01f24aa10815c4c7aaaf8688091
jinit <jparikh9@hawk.iit.edu>
2023-07-07 14:59:01 -0500

    new expense input form.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,7 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 function ExpenseItem(props) {
     //const expenseDate = new Date(2023, 6, 24);
@@ -9,12 +10,19 @@ function ExpenseItem(props) {
     //const month = props.date.toLocaleString('en-US', { month: 'long' });
     //const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     //const year = props.date.getFullYear();
+    const [title,setTitle] = useState(props.title);
+    const clickHandler = () =>{
+        setTitle('Updated!');
+        console.log(title);
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
     );
